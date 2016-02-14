8857fdf32a7232c94d890f118158496661a717db
AlexSchechter <a.schechter@outlook.com>
2016-02-14 17:48:48 -0500

    Added Households View/Model/Controlleraction

---
+++ Budgeter/Models/HouseholdViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Budgeter.Models
+{
+    public class HouseholdViewModel
+    {
+        public Household CurrentHousehold { get; set; }
+        public List<Household> HouseholdOptions { get; set; }
+    }
+}
